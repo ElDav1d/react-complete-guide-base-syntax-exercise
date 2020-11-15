7e5f2a4a0af3bee26b1288fc5d23c2175818634d
David Vivó Palanca <david.vivo@nexum.com>
2020-11-15 12:27:35 +0100

    Add state to the App component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
+import React, { Component } from 'react';
 import './App.css';
 import UserInput from './UserInput/UserInput';
 import UserOutput from './UserOutput/UserOutput';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Lets' start mate!</h1>
-      <UserInput />
-      <UserOutput userName="user 1"/>
-      <UserOutput userName="user 2"/>
-      <UserOutput userName="user 3"/>
-    </div>
-  );
+class App extends Component {
+  state = {
+    userNames: [
+      { userName: 'user 1' },
+      { userName: 'user 2' },
+      { userName: 'user 3' },
+    ]
+  }
+  render () {
+    return (
+      <div className="App">
+        <h1>Lets' start mate!</h1>
+        <UserInput />
+        <UserOutput userName={this.state.userNames[0].userName}/>
+        <UserOutput userName={this.state.userNames[1].userName}/>
+        <UserOutput userName={this.state.userNames[2].userName}/>
+      </div>
+    );
+  }
 }
 
 export default App;
