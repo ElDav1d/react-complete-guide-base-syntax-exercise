44aa32a5667e7f3497aa36b49ff12c6455b3f0a5
David Vivó Palanca <david.vivo@nexum.com>
2020-11-15 18:32:22 +0100

    Apply inlined style

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -1,11 +1,18 @@
 const userInput = (props) => {
+  const inputStyle = {
+    display: 'block',
+    margin: '1rem auto',
+  }
+
   return (
-    <div>
+    <form>
+      <label>Input a new username</label>
       <input
+        style={inputStyle}
         onChange={props.changed}
         type="text"
         value={props.name}/>
-    </div>
+    </form>
   )
 };
 
