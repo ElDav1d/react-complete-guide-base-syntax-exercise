d6c5079cd77765df492008b1a4f08c07378d78a9
David Vivó Palanca <david.vivo@nexum.com>
2020-11-15 18:34:15 +0100

    Import react at stateless components
    Although not needed because v17
    
    God to keep this habit towards legacy code

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const userInput = (props) => {
   const inputStyle = {
     display: 'block',
--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './UserOutput.css';
 
 const userOutput = (props) => {
